f327d9c7c3e841ba06b4683429f840b7823c4ddf
Łukasz Piątkowski <piontec@gmail.com>
2024-04-23 09:33:42 +0200

    fix: Chart.yaml conflict resolution added in post-hook.sh

--- post-hook.sh
+++ post-hook.sh
@@ -6,6 +6,8 @@ CHART_PATH=helm/external-secrets
 FILE=Chart.yaml
 CRD_PATH=$CHART_PATH/crd-base
 FILE_PATH=$CHART_PATH/$FILE
+OURS=/tmp/"$FILE".ours
+THEIRS=/tmp/"$FILE".theirs
 
 wget https://github.com/external-secrets/external-secrets/releases/download/helm-chart-"${VER}"/external-secrets-"${VER}".tgz -O /tmp/external-secrets.tgz
 tar zvxf /tmp/external-secrets.tgz -C /tmp
@@ -22,4 +24,14 @@ for f in $(ls -I "cluster*.yaml" "$CRD_PATH"); do
 done
 
 echo "Patching $FILE with upstream versions info"
-yq ".upstreamChartVersion = \"$VER\"" -i "$FILE_PATH"
+if git status --short "$FILE_PATH" | grep "^UU "; then
+	git show :2:"$FILE_PATH" >"$OURS"
+	git show :3:"$FILE_PATH" >"$THEIRS"
+
+	yq ".appVersion = load(\"$THEIRS\").appVersion" -i "$OURS"
+	yq ".version = load(\"$THEIRS\").version" -i "$OURS"
+	yq ".upstreamChartVersion = load(\"$THEIRS\").version" -i "$OURS"
+	cp "$OURS" "$FILE_PATH"
+else
+	yq ".upstreamChartVersion = \"$VER\"" -i "$FILE_PATH"
+fi
